6f3d78a5d316e1a1078a144d35a24e4bf1b6b43e
William Kay Kephart <wllmkphrt@gmail.com>
2022-10-19 16:14:32 -0400

    commit tree b9246a117ab92134bf6fdf09099c2d7929fd3d4f

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+function isWin(element){
+    return(element.result === "W");
+}
+function superbowlWin(record){
+    if (record.find(isWin) === undefined){
+        return undefined;
+    }
+    else {return record.find(isWin).year};
+}
